2990d124d3bfe2e2a46cf9598dde2f56a8593eb2
bybLik04 <106867943+bybLik04@users.noreply.github.com>
2023-12-07 12:19:32 +0300

    Добавьте файлы проекта.

---
+++ linq_rykhlov/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq_rykhlov
+{
+    internal class Student
+    {
+        public int ID;
+        public string Name;
+        public int Age;
+        public DateTime Birthate;
+    }
+}
